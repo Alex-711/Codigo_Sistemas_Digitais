7b7aef4700770524781f264d616c41e795c7ddef
AlexBayerlein <85767662+Alex-711@users.noreply.github.com>
2023-07-31 17:17:30 -0300

    Update fp_adder.vhd

--- fp_adder.vhd
+++ fp_adder.vhd
@@ -1,4 +1,3 @@
--- Listing 3.19
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
@@ -14,17 +13,16 @@ entity fp_adder is
 end fp_adder ;
 
 architecture arch of fp_adder is
-   -- suffix b, s, a, n for
-   --        big, small, aligned, normalized number
+
    signal signb, signs: std_logic;
    signal expb, exps, expn: unsigned(3 downto 0);
    signal fracb, fracs, fraca, fracn: unsigned(7 downto 0);
    signal sum_norm: unsigned(7 downto 0);
    signal exp_diff: unsigned(3 downto 0);
-   signal sum: unsigned(8 downto 0); --one extra for carry
+   signal sum: unsigned(8 downto 0);
    signal lead0: unsigned(2 downto 0);
 begin
-   -- 1st stage: sort to find the larger number
+
    process (sign1, sign2, exp1, exp2, frac1, frac2)
    begin
       if (exp1 & frac1) > (exp2 & frac2) then
@@ -44,7 +42,7 @@ begin
       end if;
    end process;
 
-   --  2nd stage: align smaller number
+
    exp_diff <= expb - exps;
    with exp_diff select
       fraca <=
@@ -58,12 +56,12 @@ begin
          "0000000" & fracs(7)          when "0111",
          "00000000"                    when others;
 
-   -- 3rd stage: add/substract
+
    sum <= ('0' & fracb) + ('0' & fraca) when signb=signs else
           ('0' & fracb) - ('0' & fraca);
 
-   -- 4th stage: normalize
-   -- count leading 0s
+
+
    lead0 <= "000" when (sum(7)='1') else
             "001" when (sum(6)='1') else
             "010" when (sum(5)='1') else
@@ -72,7 +70,7 @@ begin
             "101" when (sum(2)='1') else
             "110" when (sum(1)='1') else
             "111";
-   -- shift significand according to leading 0
+
    with lead0 select
       sum_norm <=
          sum(7 downto 0)             when "000",
@@ -84,14 +82,13 @@ begin
          sum(1 downto 0) & "000000"  when "110",
          sum(0) &          "0000000" when others;
 
-   -- normalize with special conditions
    process(sum,sum_norm,expb,lead0)
    begin
-      if sum(8)='1' then -- w/ carry out; shift frac to right
+      if sum(8)='1' then 
          expn <= expb + 1;
          fracn <= sum(8 downto 1);
-      elsif (lead0 > expb) then  -- too small to normalize;
-         expn <= (others=>'0');  -- set to 0
+      elsif (lead0 > expb) then  
+         expn <= (others=>'0');  
          fracn <= (others=>'0');
       else
          expn <= expb - lead0;
@@ -99,7 +96,6 @@ begin
       end if;
    end process;
 
-   -- form output
    sign_out <= signb;
    exp_out <= std_logic_vector(expn);
    frac_out <= std_logic_vector(fracn);
